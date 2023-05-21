c4baa7bbc564548382f9379335e0b73771f3be76
gilbert-ku <gilbert45ku@gmail.com>
2023-05-21 16:48:07 +0300

    add images to javaScript

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ const timeValue = document.getElementById("timer");
 const startButton = document.getElementById("start");
 const resultsButton = document.getElementById("results");
 const stopButton = document.getElementById("stop");
-const gameContainer = document.getElementsByClassName("game");
+const gameContainer = document.getElementsByClassName(".game");
 
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+const itemsImges = [
+    { name: "apple", image: "./images/apple.png"},
+    { name: "ball", image: "./images/ball.jpg"},
+    { name: "chrome", image: "./images/chrome.png"},
+    { name: "box", image: "./images/box.jpn"},
+    { name: "folder", image: "./images/folder.png"},
+    { name: "github", image: "./images/github.png"},
+
+];
