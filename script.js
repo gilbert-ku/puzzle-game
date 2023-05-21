505c2a57c52085844d313d458d10e2703a093a07
gilbert-ku <gilbert45ku@gmail.com>
2023-05-21 15:09:41 +0300

    add get element by id and class to javaScript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // alert('working');
+
+const moves = document.getElementById("moves");
+const timeValue = document.getElementById("timer");
+const startButton = document.getElementById("start");
+const resultsButton = document.getElementById("results");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.getElementsByClassName("game");
+
