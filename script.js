55b6cb3e3c75605afc78bbdb771c287c69fed297
gilbert-ku <gilbert45ku@gmail.com>
2023-05-23 10:00:30 +0300

    add game state

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 // alert('working');
 
-const moves = document.getElementById("moves");
-const timeValue = document.getElementById("timer");
-const startButton = document.getElementById("start");
-const resultsButton = document.getElementById("results");
-const stopButton = document.getElementById("stop");
-const gameContainer = document.getElementsByClassName(".game");
-
-let cards;
-let interval;
-let firstCard = false;
-let secondCard = false;
-
-const itemsImges = [
+const tilesContainer = document.querySelector(".tiles");
+const images = [
     { name: "apple", image: "./images/apple.png"},
     { name: "ball", image: "./images/ball.jpg"},
     { name: "chrome", image: "./images/chrome.png"},
@@ -21,25 +10,11 @@ const itemsImges = [
     { name: "github", image: "./images/github.png"},
 
 ];
-let seconds = 0,
-minutes = 0;
+const imagesPicklist = [...images, ...images];
+const tileCount = imagesPicklist.length;
 
-let movesCount = 0,
-winCount = 0;
-
-const timeGenerator = () => {
-    seconds+-1; 
-    if (minutes >= 60) {
-        minutes += 1;
-        seconds = 0
-    }
-};
-
-let secondsValue = seconds < 10 ? `0${seconds}` :
-seconds;
-let minutesValue = minutes < 10 ? `0${minutes}` :
-minutes;
-timeValue.innerHTML = `<span>Time: </span>${minutesValue}:${secondsValue}` ;
-
-
+// Game state
+let revealedCount = 0;
+let activeTile = null;
+let awaitingEndOfMove = false;
 
